cd449739911c087677fafac41554052a48f0158f
WASH_UR_tak <wasi9shayanno@gmail.com>
2025-12-25 22:01:26 +0600

    test1

--- DisasterDonationReliefManagementSystem/Program.cs
+++ DisasterDonationReliefManagementSystem/Program.cs
@@ -11,7 +11,7 @@ namespace DisasterDonationReliefManagementSystem
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new LogInPage());
+            Application.Run(new HomePage());
         }
     }
 }
